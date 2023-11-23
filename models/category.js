b8f9df3a1c21a64b3bf0a7e9c23e56c7935853d7
mokautsar30 <mohammadkautsar30@gmail.com>
2023-11-23 21:28:35 +0700

    revisi skeleton and controller

---
+++ models/category.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Category.hasMany(models.Course, {as: "Course"});
+    }
+    static async searchByName(val){
+      let categories = await Category.findAll({
+        include: {
+                  association: "Course",
+                  attributes: ["id", "title", "description", "CategoryId"]  
+              }
+      });
+      if(val){
+        categories = await Category.findAll({
+          where:{
+            category:{
+              [Op.iLike] : `%${val}%`
+            }
+          },
+          include: {
+                    association: "Course",
+                    attributes: ["id", "title", "description", "CategoryId"]  
+                }
+        })
+      }
+
+      return categories;
+    }
+  }
+  Category.init({
+    category: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+  });
+  return Category;
+};
---
+++ routes/index.js
@@ -0,0 +1,50 @@
+const Controller = require('../Controllers/Controller');
+
+const router = require('express').Router();
+
+router.get('/', Controller.landing);
+
+router.get('/register', Controller.SignUpForm);
+
+router.post('/register', Controller.SignUpRegister);
+
+router.get('/login', Controller.login);
+
+router.post('/login', Controller.postLogin);
+
+router.get('/logout', Controller.getLogout);
+
+router.use(function (req, res, next) {
+    if(req.session.UserId){
+        next();
+    }else{
+        const error = "Please login first!!";
+        res.redirect(`/login?error=${error}`);
+    }
+});
+const isRole = function (req, res, next) {
+    if(req.session.role === "instructor"){
+        next();
+    }else{
+        const error = "You have no acces";
+        res.redirect(`/login?error=${error}`);
+    }
+};
+
+
+
+router.get("/home", Controller.home);   //ke home dg data kategori
+
+router.get("/course/:id", Controller.coursePage); // ke home dengan detail course, video
+
+router.get("/course/edit/:id", isRole, Controller.editForm);    //ke form edit
+
+router.post("/course/edit/:id", isRole, Controller.updateForm);
+
+router.get("/course/delete/:id", isRole, Controller.deleteCourse);
+
+
+
+
+
+module.exports = router;
